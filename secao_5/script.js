e9ccf625e65716c1b65ba4f343c58960e37ae80c
joaolucas <joaolucasdeassis.o@gmail.com>
2023-01-26 18:24:56 -0300

    adicionando arquivos da secao 5

---
+++ secao_5/script.js
@@ -0,0 +1,6 @@
+const headerNav = document.querySelector("#header__nav");
+const links = document.querySelectorAll(".header__nav__links li");
+
+function clicked() {
+    hamburgerContainer.classList.toggle(".clicked");
+}
